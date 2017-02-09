c3eaa120ca3a9f76ce514537ef0737787b6bf984
Steve Kotsopoulos <sk@architech.ca>
2017-02-09 16:24:57 -0500

    add commerce-website application

---
+++ commerce-website/src/main/java/com/example/CommerceWebsiteApplication.java
@@ -0,0 +1,82 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@EnableZuulProxy
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CommerceWebsiteApplication {
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceWebsiteApplication.class, args);
+	}
+}
+
+@RestController
+@RequestMapping("/promotions")
+class PromotionServiceApiGatewayRestController {
+
+	private final RestTemplate restTemplate;
+	
+	@Autowired
+	public PromotionServiceApiGatewayRestController(@LoadBalanced RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+	
+	public Collection<String> fallbackPromotions() {
+		ArrayList<String> fallback = new ArrayList<String>();
+		fallback.add("25% off, for a limited time only");
+		return fallback;
+	}
+	
+	@HystrixCommand(fallbackMethod = "fallbackPromotions")
+	@RequestMapping(method = RequestMethod.GET, value = "/names")
+	public Collection<String> names() {
+		return this
+				.restTemplate
+				.exchange("http://promotions-service/promotions",
+						HttpMethod.GET, null,
+						new ParameterizedTypeReference<Resources<Promotion>>() {
+						})
+				.getBody()
+				.getContent()
+				.stream()
+				.map(Promotion::getName)
+				.collect(Collectors.toList());
+	}
+}
+
+class Promotion {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+}
